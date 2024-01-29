25e56f4f6aea5288a2cb2096aea8c6f1d1ce376c
nighthawk-ops <mmmkzera@gmail.com>
2024-01-29 09:18:51 +0300

    Game files
    The games files for the basic setup of the game. Movement, jumping, combat mechanics

---
+++ Game/character.py
@@ -0,0 +1,85 @@
+import pygame
+
+class Player():
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, 128, 128)
+        self.vel_y = 0
+        self.jump = False
+        self.attacking = False
+        self.attackType = 0
+        self.health = 100
+        self.flip = False
+        
+    
+    def movePlayer(self, window, target):
+        velocity = 5                    # speed of player
+        dx = 0                          # change in x coordinate
+        dy = 0                          # change in y coordinate
+        
+        gravity = 2
+        keys = pygame.key.get_pressed() # gets keys pressed
+        
+        # movement only possible if not attacking
+        if self.attacking == False:
+            #movement
+            if keys[pygame.K_a]:            # press a to go left
+                dx = -velocity
+            if keys[pygame.K_d]:            # press b to go right
+                dx += velocity
+            
+            #jumping
+            if keys[pygame.K_w] and self.jump == False: # Press w to jump
+                self.vel_y = -30
+                self.jump = True
+                
+            # attacking
+            if keys[pygame.K_r] or keys[pygame.K_t]:  # press T or R to attack
+                self.attack(window, target)
+                if keys[pygame.K_r]:                  
+                    self.attackType = 1               # attack type 1 if r is pressed
+                if keys[pygame.K_t]:
+                    self.attackType = 2               # attack type 1 if r is pressed
+            
+            
+        #update position
+        self.vel_y += gravity           # applies gravity to bring character back down  
+        dy += self.vel_y                # updates y
+        
+        self.rect.x += dx               # horizontal position
+        self.rect.y += dy               # vertical position
+        
+        # ensure players face each other
+        if target.rect.centerx > self.rect.centerx: # check if opponent is to the right
+            self.flip = False
+        else:
+            self.flip = True 
+        
+         # Boarder check: resets position if crossed.
+        if self.rect.x <= 0:            # left boarder
+             self.rect.x = 0            # reset coordinate
+        elif self.rect.x>= 672:         # right boarder
+            self.rect.x = 672           # reset coordinate
+        
+        if self.rect.y >= 468:          # ensures character doesn't fall through floor
+            self.vel_y = 0
+            self.rect.y = 468
+            self.jump = False
+        
+        
+    
+    def attack(self, window, target):
+        self.attacking = True
+        hitbox = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+        #check for contact
+        if hitbox.colliderect(target.rect):           #checks for collision to determine effect
+            target.health -= 10
+        
+        pygame.draw.rect(window, (0,255,0), hitbox)   #draws hitbox
+        
+        
+    
+    def drawPlayer(self, window):
+        pygame.draw.rect(window, (0,0,0), self.rect)
+
+    
+  
---
+++ Game/game.py
@@ -0,0 +1,73 @@
+import pygame
+from character import Player        # get player code to be used in game loop
+
+# initializes game
+pygame.init()
+
+# window variables
+windowWidth = 800
+windowHeight = 640 
+window = pygame.display.set_mode((windowWidth,windowHeight))
+title = pygame.display.set_caption("Battle-Bytes")
+clock = pygame.time.Clock()
+icon = pygame.image.load("assests/images/icon/muay-thai.png")   # variabe that holds the icon image.
+pygame.display.set_icon(icon) # loads the icon onto the window.
+
+#background image (images should be loaded outside main loop)
+bgImage = pygame.image.load("assests/images/background/dojo.png").convert_alpha()
+def draw_bg():
+    scaledBg = pygame.transform.scale(bgImage, (windowWidth, windowHeight)) # scales bg to screen size
+    window.blit(scaledBg, (0,0))
+
+# load player images
+playerOne_img = pygame.image.load("assests/images/character/demo/action.png").convert_alpha()
+playerTwo_img = pygame.image.load("assests/images/character/demo/man.png").convert_alpha()
+
+#Colors
+RED = (255, 0, 0)   
+DARKRED = (132, 22, 23)
+WHITE = (255,255,255)
+
+
+# Players health bar
+def healthBar(health, x, y):
+    ratio = health/100
+    pygame.draw.rect(window, WHITE, (x-1, y-1, 252, 27))
+    pygame.draw.rect(window, DARKRED, (x, y, 250, 25))
+    pygame.draw.rect(window, RED, (x, y, 250*ratio, 25)) 
+
+# player instances
+playerOne = Player(80, 468)
+playerTwo = Player(580, 468)
+
+#Game loop
+run = True
+while run:
+    
+    clock.tick(60)                      # setting FPS
+    
+    window.fill((14,219,240))           # screen background
+    draw_bg()                           # draws background image
+    
+    #move players
+    playerOne.movePlayer(window,playerTwo) 
+    
+    #draw player stats
+    healthBar(playerOne.health, 20, 20)
+    healthBar(playerTwo.health, 520, 20)
+    
+    #draw fighters
+    playerOne.drawPlayer(window,playerOne_img, 80, 468)
+    playerTwo.drawPlayer(window, playerTwo_img, 580, 468)
+    
+    
+    #Event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:   # quit event when x in top corner pressed.
+            run = False
+    
+    
+    pygame.display.update()             # updates screen inorder to see changes.
+
+
+pygame.quit()                           # quit pygame.
